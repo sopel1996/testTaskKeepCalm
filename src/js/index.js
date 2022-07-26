ceb41b23a8b4aec7aa6edff07dc5c66e2a81099c
sopel1996 <sopel@live.ru>
2022-07-26 22:35:55 +0300

    add media

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,31 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: false,
-    centeredSlides: true,
-    slidesPerView: 1.5,
-    spaceBetween: 30 ,
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: false,
+  centeredSlides: true,
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      // spaceBetween: 300,
+    },
+    992: {
+      slidesPerView: 3,
+      // spaceBetween: 00,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
 });
 
 Fancybox.bind('[data-fancybox="gallery"]', {
-    dragToClose: false,
-    Thumbs: false,
-    Toolbar: false,
-    closeButton: "top",
-  });
+  dragToClose: false,
+  Thumbs: false,
+  Toolbar: false,
+  closeButton: "top",
+});
